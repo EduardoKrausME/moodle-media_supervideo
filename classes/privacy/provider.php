0f78df3755dc43da2f641aec6bf29ffe6e55579c
Eduardo Kraus <kraus@eduardokraus.com>
2025-08-29 15:01:33 -0300

    copyright

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -18,7 +18,7 @@
  * Privacy provider implementation for media_supervideo.
  *
  * @package   media_supervideo
- * @copyright 2024 Eduardo kraus (http://eduardokraus.com)
+ * @copyright 2024 Eduardo Kraus {@link https://eduardokraus.com}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
